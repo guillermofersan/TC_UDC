6790613d16e5f6aca3b80e17a6b18028f3c6c21e
Guillermo <guillermofersan@gmail.com>
2023-04-02 21:09:54 +0200

    Ejercicio 1 practica 1

--- p1/ejercicio1.ml
+++ p1/ejercicio1.ml
@@ -13,51 +13,29 @@ Arco_af (Estado "1", Estado "1", Terminal "b");
 Arco_af (Estado "1", Estado "2", Terminal "a");
 Arco_af (Estado "2", Estado "0", Terminal "a");
 Arco_af (Estado "2", Estado "3", Terminal "c")],
-Conjunto [Estado "1"; Estado "3"])
+Conjunto [Estado "1"; Estado "3"]);;
 
 
 *****************************************************************************
  *
- * Un aut�mata finito viene definido por la 5-tupla AF = (Q,E,q0,d,F) donde: 
+ * Un automata finito viene definido por la 5-tupla AF = (Q,E,q0,d,F) donde: 
  *    Q  = conjunto de estados      -> estado conjunto                       
  *    E  = alfabeto de entrada      -> simbolo conjunto                      
  *    q0 = estado inicial           -> estado                                
- *    d  = funci�n  de transici�n   -> arco_af conjunto                      
+ *    d  = funcion  de transicion   -> arco_af conjunto                      
  *    F  = conjunto estados finales -> estado conjunto                       
  *
  *****************************************************************************)
 
-
-(*
-función es_afne(automata):
-  para cada estado en el automata:
-    para cada transición en las transiciones del estado:
-      si el símbolo de entrada de la transición es epsilon:
-        devolver true
-  para cada estado en el automata:
-    para cada transición en las transiciones del estado:
-      si el símbolo de entrada de la transición no es epsilon:
-        si el estado de destino tiene épsilon-transiciones:
-          devolver true
-  devolver false
-*)
 let es_afne (Af (estados, simbolos, e_ini, arcos, e_fin)) = 
   let rec aux = function 
       Conjunto [] -> false
     | _ -> true
   in aux (avanza (Terminal "") estados (Af (estados, simbolos, e_ini, arcos, e_fin)));;
+(*si presenta una epsilon transición, podemos afirmar que se trata de un afne*)
 
 
 (*
-función es_afn(automata):
-  para cada estado en el automata:
-    para cada transición en las transiciones del estado:
-      si hay algún símbolo de entrada duplicado en las transiciones del estado:
-        devolver true
-  devolver false
-  *)
-
-  (*
 Af (Conjunto [Estado "0"; Estado "1"; Estado "2"; Estado "3"],
 Conjunto [Terminal "a"; Terminal "b"; Terminal "c"],
 Estado "0",
@@ -68,15 +46,11 @@ Arco_af (Estado "2", Estado "0", Terminal "a");
 Arco_af (Estado "2", Estado "3", Terminal "a");
 Arco_af (Estado "2", Estado "3", Terminal "c")],
 Conjunto [Estado "1"; Estado "3"]);;
-
-
-let get_transitions estado (Conjunto arcos) =
-  let rec aux arcs transiciones = match arcs with 
-      [] -> transiciones
-    | (Arco_af (e, d, s))::t -> if (e=estado) then aux t (Arco_af (e, d, s)::transiciones) else (aux t transiciones)
-  in aux arcos [];;
 *)
-let get_simbols estado (Conjunto arcos) =
+
+(*devuelve una lista de simbolos de las transiciones salientes del estado, 
+  un simbolo puede estar repetidossi hay mas de una transicion con dicho simbolo*)
+let get_simbols estado (Conjunto arcos) = 
   let rec aux arcs sim = match arcs with 
       [] -> sim
     | (Arco_af (e, d, s))::t -> if (e=estado) then (aux t (s::sim)) else (aux t sim)
@@ -93,24 +67,34 @@ let es_afn (Af (Conjunto estados, simbolos, e_ini, arcos, e_fin)) =
 
 
 (*
-funcion es_afd(automata):
-  simbolos_entrada_inicial = lista de símbolos de entrada de las transiciones del estado inicial del automata
-  estados_visitados = [estado inicial del automata]
-  para cada estado en estados_visitados:
-    transiciones_estado = lista de transiciones del estado en el automata
-    simbolos_entrada = lista de simbolos de entrada de las transiciones_estado
-    si hay algún símbolo de entrada duplicado en simbolos_entrada:
-      devuelve false
-    para cada símbolo de simbolos_entrada:
-      destino = estado de destino de la transición correcpondiente al símbolo
-      si el destino no es un estado del autómata:
-        devolver false
-      si el destino no está en estados_visitados:
-        añadir destino a estados_visitados
-  devolver true   
+Af (Conjunto [Estado "0"; Estado "1"; Estado "2"; Estado "3"],
+Conjunto [Terminal "a"; Terminal "b"],
+Estado "0",
+Conjunto [Arco_af (Estado "0", Estado "1", Terminal "a");
+Arco_af (Estado "0", Estado "0", Terminal "b");
+Arco_af (Estado "1", Estado "1", Terminal "b");
+Arco_af (Estado "1", Estado "2", Terminal "a");
+Arco_af (Estado "2", Estado "0", Terminal "a");
+Arco_af (Estado "3", Estado "3", Terminal "a");
+Arco_af (Estado "3", Estado "3", Terminal "b");
+Arco_af (Estado "2", Estado "3", Terminal "b")],
+Conjunto [Estado "1"; Estado "3"])
 *)
 
 let es_afd (Af (Conjunto estados, simbolos, e_ini, arcos, e_fin)) =
-  let simbolos_entrada_inicial = get_simbols (e_ini, arcos) in
-  let estados_visitados = [e_ini] in 
+  let rec aux estados_por_visitar estados_visitados = match estados_por_visitar with
+      [] -> true 
+    | h::t-> let simbols = (get_simbols h arcos) in 
+             if not ((List.length simbols) = (cardinal (conjunto_of_list(simbols)))) then false (*comprobación de si hay simbolos repetidos*)
+             else if not (igual (conjunto_of_list(simbols)) simbolos) then false
+             else let rec aux2 simbols2 estados_por_visitar_temp estados_visitados_temp = match simbols2 with
+                      [] -> aux estados_por_visitar_temp estados_visitados_temp (*???*)
+                    | h1::t1 -> let destino = avanza h1 (Conjunto [h]) (Af (Conjunto estados, simbolos, e_ini, arcos, e_fin)) in
+                              if (cardinal destino) != 1 then false else let estadoDestino = (List.hd (list_of_conjunto(destino))) in (*necesario?*)
+                                 if not (pertenece estadoDestino (Conjunto estados)) 
+                                    then false 
+                                    else if (List.mem estadoDestino estados_visitados_temp) then aux2 t1 estados_por_visitar_temp estados_visitados_temp
+                                                                                       else aux2 t1 (estadoDestino::estados_por_visitar_temp) (estadoDestino::estados_visitados_temp) 
+                  in aux2 simbols t estados_visitados
+  in aux [e_ini] [e_ini];;
   
